6423470ab6de95b2c62d96034374c04efaf41c3b
ZedZagg <70690118+ZedZagg@users.noreply.github.com>
2020-09-09 21:49:15 +1000

    Add files via upload

---
+++ AlbumArtShowcase.js
@@ -0,0 +1,74 @@
+﻿window.DefinePanel("AlbumArtShowcase", { author: "ZedZagg" });
+include(`${fb.ComponentPath}docs\\Flags.js`);
+include(`${fb.ComponentPath}docs\\Helpers.js`);
+
+const imgCount = 4;
+const noArtImg = gdi.Image("D:/sample.jpg");
+const spacing = 2;
+const bgCol = RGB(255, 255, 255)
+
+var imgSize;
+var imageSet = new Array(imgCount);
+
+function on_paint(gr) {
+    gr.FillSolidRect(0, 0, window.Width, window.Height, bgCol);
+    
+    var i;
+    for (i = 0; i < imgCount; i++) {
+        drawArt(i * (imgSize + spacing),imageSet[i],gr);
+    }
+}
+
+function drawArt(xPos,img,gr){
+    if(!img){
+        if(noArtImg){
+            gr.DrawImage(noArtImg, xPos, 0, imgSize, imgSize, 0, 0, noArtImg.Width, noArtImg.Height);
+        }
+        else{
+            // change the RGB(16, 16, 16) for different default no art image colours
+            gr.FillSolidRect(xPos, 0, imgSize, imgSize, RGB(16, 16, 16));
+            gr.DrawLine(xPos, imgSize, xPos + imgSize, 0, 3, bgCol)
+        }
+    }
+    else{
+        gr.DrawImage(img, xPos, 0, imgSize, imgSize, 0, 0, img.Width, img.Height);
+    }
+}
+
+function on_size() {
+    var len = (window.Width - (imgCount - 1) * spacing)/imgCount;
+    
+    if(len < window.Height)
+        imgSize = len;
+    else
+        imgSize = window.Height;
+}
+
+function on_playback_new_track(metadb) {
+    var tfo = fb.TitleFormat("%title%\n%artist%\n%album%");
+        console.log(tfo.EvalWithMetadb(metadb));
+    getArt();    
+}
+
+function getArt(){
+    var handles = plman.GetPlaylistItems(plman.PlayingPlaylist);
+
+    var playing_item_location = plman.GetPlayingItemLocation();
+    var index = playing_item_location.PlaylistItemIndex;
+    
+    handleCount = handles.Count - index;
+            
+    if (handleCount == 0){
+        return;
+    }
+
+    var i;
+    for (i = 0; i < imgCount; i++) {
+        if (i == handleCount){
+            break;
+        }
+        imageSet[i] = utils.GetAlbumArtV2(handles[index + i], AlbumArtId.front);
+    }
+
+    window.Repaint();
+}
---
+++ ReadMe.md
@@ -0,0 +1,5 @@
+This script for foobar2000's Spider Monkey Panel plugin displays the album art for current track followed by any enqueued.
+The number of displayed tracks is set via the imgCount on line 5.
+The art shown for tracks that don't have any can be customized on line 6. If no art is set a default one will be used.
+The spacing between arts is set via spacing on line 7.
+Background color is set via bgCol on line 8.
